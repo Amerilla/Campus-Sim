e802b06360d6fc337ac8dae39a2c8a72ef8e9df6
Armonis5 <97055469+Armonis5@users.noreply.github.com>
2023-04-25 14:59:10 +0200

    Buldings modif
    Adding constructor to building, adding behaviour and buildings class

---
+++ Assets/Scripts/Game/BuildingBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class BuildingBehaviour:MonoBehaviour
+    {
+        private BuildingStats _buildingStats;
+        
+        void Start() {
+            
+        }
+
+        void Update() {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Game/BuildingStats.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+    public class BuildingStats
+    {
+        private string _name;
+        private List<Score> _scores;
+        private int _cost;
+        private int _capacity;
+        private int _resaleValue;
+        private string _description;
+        private Owner _owner;
+        private State _state;
+        private Type _type;
+
+        public BuildingStats(string name, List<Score> scores, int cost, int capacity, int resaleValue, string description, Owner owner, State state, Type type) {
+            _name = name;
+            _scores = scores;
+            _cost = cost;
+            _capacity = capacity;
+            _resaleValue = resaleValue;
+            _description = description;
+            _owner = owner;
+            _state = state;
+            _type = type;
+        }
+
+        public enum State
+        {
+            Good, Ok, Abandoned
+        }
+        public enum Type
+        {
+            Transportation, Housing, Education, Parking, Park, Misc
+        }
+
+    }
+    
+}
