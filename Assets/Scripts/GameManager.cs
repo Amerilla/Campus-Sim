b236eb806cfdd1b164333c09fcc602c2d6cf1ef0
Armonis5 <97055469+Armonis5@users.noreply.github.com>
2023-05-02 19:23:10 +0200

    Almost Done, except object creation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,29 @@ public class GameManager : MonoBehaviour
 {
     private Dictionary<string, BuildingBehaviour> _buildings = new();
     private Dictionary<string, GameObject> _gameObjects = new();
-
+    private int _currentTurn;
+    private Campus _campus;
     // Start is called before the first frame update
     void Start() {
+        
         foreach (string s in Buildings.names) {
             _gameObjects.Add(s,GameObject.Find(s));
             _buildings.Add(s,GameObject.Find(s).GetComponent<BuildingBehaviour>());
         }
+        _campus = new("EPFL-UNIL", 0, 0, 0, 0, 0,
+            Campus.State.Neutral);
     }
 
     // Update is called once per frame
     void Update() {
         
+    }
+
+    public void NextTurn() {
+
+
+
+
+        _currentTurn++;
     }
 }
