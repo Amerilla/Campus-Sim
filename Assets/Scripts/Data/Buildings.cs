9843fc59288319cedc9da312e8fa379f1badd4d4
Armonis5 <97055469+Armonis5@users.noreply.github.com>
2023-05-02 17:41:25 +0200

    GameManager

--- Assets/Scripts/Data/Buildings.cs
+++ Assets/Scripts/Data/Buildings.cs
@@ -6,24 +6,30 @@ namespace Assets.Scripts.Data
 {
     public static class Buildings
     {
-        public static BuildingStats EPFL_IN = new BuildingStats("EPFL_IN", new List<Score>(), 0, 0, 0,
+        private static BuildingStats EPFL_IN = new BuildingStats("EPFL_IN", new List<Score>(), 0, 0, 0,
             "", null, BuildingStats.State.Good, BuildingStats.Type.Education);
 
-        public static BuildingStats EPFL_BM = new BuildingStats("EPFL_BM", new List<Score>(), 0, 0, 0,
+        private static BuildingStats EPFL_BM = new BuildingStats("EPFL_BM", new List<Score>(), 0, 0, 0,
             "", null, BuildingStats.State.Good, BuildingStats.Type.Education);
 
-        public static BuildingStats EPFL_CM = new BuildingStats("EPFL_CM", new List<Score>(), 0, 0,
+        private static BuildingStats EPFL_CM = new BuildingStats("EPFL_CM", new List<Score>(), 0, 0,
             0, "", null, BuildingStats.State.Good, BuildingStats.Type.Education);
 
         public static Dictionary<string, BuildingStats> get = new Dictionary<string, BuildingStats>() {
 
-            { "IN",EPFL_IN},
-            {"BM",EPFL_BM},
-            {"CM",EPFL_CM},
+            { "EPFL_IN",EPFL_IN},
+            {"EPFL_BM",EPFL_BM},
+            {"EPFL_CM",EPFL_CM},
         };
 
-        private List<string> names;
-        
+        public static readonly List<string> names = new List<string>();
+
+        static Buildings() {
+            
+            foreach (var (s, buildingStats) in get) {
+                names.Add(s);
+            }
+        }
         
 
     };
