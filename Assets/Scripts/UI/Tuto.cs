ffb47dd1b269ae9dc4ded233a7f7817f132543f9
Armonis5 <97055469+Armonis5@users.noreply.github.com>
2023-08-09 15:55:52 +0200

    Tutorial added

--- Assets/Scripts/UI/Tuto.cs
+++ Assets/Scripts/UI/Tuto.cs
@@ -1,17 +1,48 @@
 
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class Tuto : MonoBehaviour
 {
+    public string _nextScene;
+    
+    private VisualElement _root;
+    private UIDocument _uiDocument;
+    private Button _next;
+    private VisualElement _global;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _uiDocument = GetComponent<UIDocument>();
+        _uiDocument.sortingOrder = 0;
+        _root = _uiDocument.rootVisualElement;
+        _global = _root.Q<VisualElement>("Global");
+        _next = _global.Q<Button>("Next");
+        _next.clicked += () => {
+            SceneManager.LoadScene(_nextScene);
+            Hide();
+        };
     }
 
-    // Update is called once per frame
-    void Update()
-    {
         
+    private void OnEnable() {
+        SceneManager.sceneLoaded += (arg0, mode) => Show();
+    }
+        
+
+
+    private void Hide() {
+        _uiDocument.sortingOrder = 0;
+        _global.visible = false;
+        _global.SetEnabled(false);
+    }
+        
+    private void Show() {
+        _global = _root.Q<VisualElement>("Global");
+        _global.visible = true;
+        _global.SetEnabled(true);
     }
 }
+
